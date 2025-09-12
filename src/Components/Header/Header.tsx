2563032abbe9dc1944ee96214f03b219c2fb01cf
Pablo Redig <pabloleaoredig@hotmail.com>
2025-09-12 16:36:33 -0300

    Ajuste no Header (incluido Logo)

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import style from "./Header.module.css"
-
+import logoPlataforma101 from "../../assets/Logo Plataforma 101.png"
 export default function Header() {
     return (
         <>
             <div className={style.container}>
                 <div className={style.img}>
-                    <img src="" alt="LogoPlataforma" />
+                    <img src={logoPlataforma101} alt="LogoPlataforma" />
                 </div>
                 <ul className={style.menu}>
                     <li> Contato </li>
