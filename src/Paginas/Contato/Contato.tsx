7191774e11b091a3551921be5f3b248dc6d17fb8
Pablo Redig <pabloleaoredig@hotmail.com>
2025-10-02 16:11:05 -0300

    Bug 001

--- src/Paginas/Contato/Contato.tsx
+++ src/Paginas/Contato/Contato.tsx
@@ -3,7 +3,6 @@ import Footer from '../../Components/Footer/Footer'
 import InstagramIcon from '@mui/icons-material/Instagram';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import style from "./Contato.module.css"
-import Loading from '../../Components/Loanding/Loading';
 export default function Contato() {
   return (
     <>
